fd744f7d4cbc67ab5da4fcc78fd7b73f28736498
Eloi Turpin <eloi.turpin@gmail.com>
2019-05-24 22:57:08 +0200

    modif exo

--- src/test/java/java8/ex07/DateAndTime_07_Test.java
+++ src/test/java/java8/ex07/DateAndTime_07_Test.java
@@ -3,7 +3,6 @@ package java8.ex07;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
-import java.time.LocalDate;
 import java.time.Month;
 import java.util.Date;
 
@@ -18,13 +17,15 @@ public class DateAndTime_07_Test {
 	public void test_date_to_localdate() throws Exception {
 
 		// TODO Créer une date Java 1 (12/02/2017)
-		Date date = null;
+		Date date = new Date(117, 2, 12);
 
 		// TODO transformer la date en Instant
+		date.toInstant();
 
 		// TODO transformer la date en LocalDate
+		date.
 
-		LocalDate result = null;
+				LocalDate result = null;
 
 		// TODO valoriser les différentes variables afin de rendre le test passant
 
